2c6c37ae31c9723a7f4f4f16022d38a0f55fb731
Johnson Zhou <johnson.zhou.sh@icloud.com>
2020-02-14 13:20:16 -0800

    DP structure changed + Serialization
    DP structure is changed to have an array of floats to implement a generic serialization method in the PacketType base class

--- Software/spartan/src/data/datapacket.h
+++ Software/spartan/src/data/datapacket.h
@@ -33,6 +33,57 @@ namespace spartan {
 
 
     // Protocol (abstract base class) to which all datapackets conform to
+    /*
+     * Pattern of output
+     * 1. Timestamp
+     * 2. Data packet array size
+     * 3. dp.data[0]
+     * ...
+     */
+    class PacketType {
+    public:
+        virtual void populate(const DataPacket & dp) = 0;
+
+        // Each sensor packet would implement this to vary data array size
+        virtual int getSize() const = 0;
+
+        // Read and write PacketType data through file stream
+        friend std::ostream & operator << (std::ostream & out, const PacketType & dp)
+        {
+            out << dp.m_timestamp << "\n";
+            out << dp.getSize() << "\n";
+            for (int i=0; i<dp.getSize(); i++)
+                out << dp.data[i] << "\n";
+            return out;
+        }
+
+        friend std::istream & operator >> (std::istream &in, PacketType & dp)
+        {
+            in >> dp.m_timestamp;
+            in.ignore(256, '\n'); // Don't really need the size
+            for (int i = 0; i < dp.getSize(); i++)
+                in >> dp.data[i];
+            return in;
+        }
+
+    protected:
+        unsigned long m_timestamp;
+        float * data;
+    };
+
+    /* [0: accel.x, 1: .y, 2: .z,
+     *  3: gyro.x, 4: .y, 5: .z,
+     *  6: temp] */
+    class IMUDataPacket : public PacketType {
+    public:
+        int getSize() const {
+            return 7;
+        }
+        virtual void populate(const DataPacket &dp);
+        // Size of data array
+    };
+
+    /* Commented to test new simplified generic structure
     class PacketType {
     public:
         virtual void populate(const DataPacket &dp) = 0;
@@ -44,6 +95,7 @@ namespace spartan {
     public:
         virtual void populate(const DataPacket &dp);
     private:
+        float m_accel_x;
         float m_accel_y;
         float m_accel_z;
         float m_gyro_x;
@@ -106,6 +158,7 @@ namespace spartan {
     };
 
     class PayloadDataPacket {};
+     */
 
 } // namespace spartan
 
