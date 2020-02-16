0ae92e43216a031ba08318da9f068df6ec859662
Johnson Zhou <johnson.zhou.sh@icloud.com>
2020-02-16 00:04:25 -0800

    Update main.cpp

--- Software/main/main.cpp
+++ Software/main/main.cpp
@@ -15,37 +15,37 @@ int main() {
     // TODO: Initialize DataPackets
     // spartan::DataPacket masterPacket;
     // dataPackets[0] = new spartan::IMUDataPacket;
+    /*
+    spartan::LSM6DS33 * l6 = dynamic_cast<spartan::LSM6DS33 *>(sensors[0]);
+    l6->powerOn();
+    l6->poll();
+    l6->printValues(); */
+    spartan::DataPacket * dp; /*
+    std::cout << l6->pollData(dp) << std::endl;
+    std::cout << * dp << std::endl;
+    std::cout << "DIVIDER" << std::endl; */
 /*
-spartan::LSM6DS33 * l6 = dynamic_cast<spartan::LSM6DS33 *>(sensors[0]);
-l6->powerOn();
-l6->poll();
-l6->printValues(); */
-spartan::DataPacket * dp; /*
-std::cout << l6->pollData(dp) << std::endl;
-std::cout << * dp << std::endl;
-std::cout << "DIVIDER" << std::endl; */
-
-sensors[0]->powerOn();
-sensors[0]->poll();
-sensors[0]->printValues();
-std::cout << sensors[0]->pollData(dp) << std::endl;
-std::cout << * dp << std::endl;
-
-//std::cout << *dp << std::endl;
-std::cout << dp->getSize() << std::endl;
+    sensors[0]->powerOn();
+    sensors[0]->poll();
+    sensors[0]->printValues();
+    std::cout << sensors[0]->pollData(dp) << std::endl;
+    std::cout << * dp << std::endl;
+*/
     // Initialization of sensors
     for (int i = 0; i < sensors.size(); i++) {
-	//sensors[i]->powerOn();
+	    sensors[i]->powerOn();
     }
     // TODO: flight loop
-    for (int i = 0; i < sensors.size(); i++) {
-        bool result = sensors[i]->pollData(dataPackets[i]);
-	if (!result)
-	    std::cerr << "Sensor name: " << sensors[i]->name() << " instance; " << sensors[i]->getInstance() << " poll data error!" << std::endl;
-    }
+    while (true) {
+        for (int i = 0; i < sensors.size(); i++) {
+            // Polls data from sensor #i
+            if (!(sensors[i]->pollData(dataPackets[i])))
+                std::cerr << "Sensor name: " << sensors[i]->name() << " instance; " << sensors[i]->getInstance() << " poll data error!" << std::endl;
 
-    std::cout << dataPackets[0]->getSize() << std::endl;
-    std::cout << *dataPackets[0] << std::endl;
+            std::cout << "Packet size " << dataPackets[i]->getSize() << std::endl;
+            std::cout << *dataPackets[i] << std::endl;
+        }
+    }
     /*
     for (int i = 0; i < dataPackets.size(); i++) {
         dataPackets[i]->populate(masterPacket);
