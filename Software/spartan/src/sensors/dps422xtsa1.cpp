82baaf8e297a93df00ed561755dae0b30c08081d
siddharthnandy <53063086+siddharthnandy@users.noreply.github.com>
2021-01-04 19:53:06 -0800

    Update dps422xtsa1.cpp

--- Software/spartan/src/sensors/dps422xtsa1.cpp
+++ Software/spartan/src/sensors/dps422xtsa1.cpp
@@ -2,6 +2,12 @@
 /*
 spartan::DPS422XTSA1::DPS422XTSA1(int bus, uint8_t address) {}
 
+const char* spartan::DPS422XTSA1::name() const { return ''; }
+void spartan::DPS422XTSA1::printSensorInfo() {}
+
+int spartan::DPS422XTSA1::powerOn() { return 0; }
+int spartan::DPS422XTSA1::powerOff() { return 0; }
+
 bool spartan::DPS422XTSA1::pollData(spartan::DataPacket &dp) { return 0; }
 
 void spartan::DPS422XTSA1::printValues() const {}
