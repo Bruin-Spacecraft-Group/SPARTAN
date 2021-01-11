8ef23ee387ca3274482a87192035e191a0cb7684
eduong100 <eduong100@gmail.com>
2021-01-10 22:16:11 -0800

    Change return type for comment outlining ADS1115's pollData function

--- Software/spartan/src/sensors/ads1115.cpp
+++ Software/spartan/src/sensors/ads1115.cpp
@@ -2,7 +2,7 @@
 /*
 spartan::ADS1115::ADS1115(int bus, uint8_t address) {}
 
-bool spartan::ADS1115::pollData(spartan::DataPacket &dp) { return 0; }
+int spartan::ADS1115::pollData(spartan::DataPacket &dp) { return 0; }
 
 void spartan::ADS1115::printValues() const {}
 */
