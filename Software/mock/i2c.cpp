22bd01c3c4b5252cf68bc740fae7579a7a742fa5
Harrison Cassar <harrison.cassar@gmail.com>
2021-01-18 19:29:48 -0800

    Fix compilation errors. -Arose because of recent addition of `-Werror` and other compile options.

--- Software/mock/i2c.cpp
+++ Software/mock/i2c.cpp
@@ -2,17 +2,28 @@
 
 // Contructors/destructors
 
-mraa::I2c::I2c(int bus, bool raw) {}
-mraa::I2c::I2c(void* i2c_context) {}
+mraa::I2c::I2c(int bus, bool raw) {
+    (void) bus;
+    (void) raw;
+}
+
+mraa::I2c::I2c(void* i2c_context) {
+    (void) i2c_context;
+}
+
 mraa::I2c::~I2c() {}
 
 // I2C interface settings
 
 mraa::Result mraa::I2c::frequency(mraa::I2cMode mode) {
+    (void) mode;
+
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::I2c::address(uint8_t address) {
+    (void) address;
+
     return mraa::SUCCESS;
 }
 
@@ -30,14 +41,20 @@ int mraa::I2c::read(uint8_t* data, int length) {
 }
 
 uint8_t mraa::I2c::readReg(uint8_t reg) {
+    (void) reg;
+
     return (uint8_t) std::rand();
 }
 
 uint16_t mraa::I2c::readWordReg(uint8_t reg) {
+    (void) reg;
+
     return (uint16_t) std::rand();
 }
 
 int mraa::I2c::readBytesReg(uint8_t reg, uint8_t* data, int length) {
+    (void) reg;
+
     for (int i = 0; i < length; i++) {
         data[i] = (uint8_t) std::rand();
     }
@@ -47,17 +64,27 @@ int mraa::I2c::readBytesReg(uint8_t reg, uint8_t* data, int length) {
 // Write functions
 
 mraa::Result mraa::I2c::writeByte(uint8_t data) {
+    (void) data;
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::I2c::write(const uint8_t* data, int length) {
+    (void) data;
+    (void) length;
+
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::I2c::writeReg(uint8_t reg, uint8_t data) {
+    (void) reg;
+    (void) data;
+
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::I2c::writeWordReg(uint8_t reg, uint16_t data) {
+    (void) reg;
+    (void) data;
+
     return mraa::SUCCESS;
 }
--- Software/mock/spi.cpp
+++ Software/mock/spi.cpp
@@ -2,40 +2,64 @@
 
 // Constructors/destructors
 
-mraa::Spi::Spi(int bus) {}
-mraa::Spi::Spi(int bus, int cs) {}
-mraa::Spi::Spi(void* spi_context) {}
+mraa::Spi::Spi(int bus) {
+    (void) bus;
+}
+
+mraa::Spi::Spi(int bus, int cs) {
+    (void) bus;
+    (void) cs;
+}
+
+mraa::Spi::Spi(void* spi_context) {
+    (void) spi_context;
+}
+
 mraa::Spi::~Spi(){}
 
 // SPI interface settings
 
 mraa::Result mraa::Spi::mode(Spi_Mode mode) {
+    (void) mode;
+
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::Spi::frequency(int hz) {
+    (void) hz;
+
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::Spi::lsbmode(bool lsb) {
+    (void) lsb;
+
     return mraa::SUCCESS;
 }
 
 mraa::Result mraa::Spi::bitPerWord(unsigned int bits) {
+    (void) bits;
+
     return mraa::SUCCESS;
 }
 
 // Read/write functions
 
 int mraa::Spi::writeByte(uint8_t data) {
+    (void) data;
+
     return std::rand();
 }
 
 int mraa::Spi::writeWord(uint16_t data) {
+    (void) data;
+
     return std::rand();
 }
 
 uint8_t* mraa::Spi::write(uint8_t* txBuf, int length) {
+    (void) txBuf;
+
     uint8_t *buf = (uint8_t *)std::malloc(length * sizeof(uint8_t));
     if (buf == NULL) {
         throw std::bad_alloc();
@@ -48,6 +72,8 @@ uint8_t* mraa::Spi::write(uint8_t* txBuf, int length) {
 }
 
 uint16_t* mraa::Spi::writeWord(uint16_t* txBuf, int length) {
+    (void) txBuf;
+
     uint16_t *buf = (uint16_t *)std::malloc(length * sizeof(uint16_t));
     if (buf == NULL) {
         throw std::bad_alloc();
@@ -60,6 +86,8 @@ uint16_t* mraa::Spi::writeWord(uint16_t* txBuf, int length) {
 }
 
 mraa::Result mraa::Spi::transfer(uint8_t* txBuf, uint8_t* rxBuf, int length) {
+    (void) txBuf;
+
     for (int i = 0; i < length; i++) {
         rxBuf[i] = (uint8_t) std::rand();
     }
@@ -67,6 +95,8 @@ mraa::Result mraa::Spi::transfer(uint8_t* txBuf, uint8_t* rxBuf, int length) {
 }
 
 mraa::Result mraa::Spi::transfer_word(uint16_t* txBuf, uint16_t* rxBuf, int length) {
+    (void) txBuf;
+
     for (int i = 0; i < length; i++) {
         rxBuf[i] = (uint16_t) std::rand();
     }
