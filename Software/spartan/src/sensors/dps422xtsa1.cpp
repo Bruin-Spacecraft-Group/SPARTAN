f955c3448dac246dbeeb1777b09c539f3d46764e
siddharthnandy <53063086+siddharthnandy@users.noreply.github.com>
2021-02-02 19:46:35 -0800

    Update dps422xtsa1.cpp

--- Software/spartan/src/sensors/dps422xtsa1.cpp
+++ Software/spartan/src/sensors/dps422xtsa1.cpp
@@ -1,4 +1,5 @@
 #include "dps422xtsa1.h"
+#include "globals.h"
 
 spartan::DPS422XTSA1::DPS422XTSA1(int bus, uint8_t address) 
 	: Sensor(bus, address), m_i2c(bus, true)
@@ -8,9 +9,8 @@ spartan::DPS422XTSA1::DPS422XTSA1(int bus, uint8_t address)
 
 const char* spartan::DPS422XTSA1::name() const
 {
-	// Implementation
-	const char* sensor_name = "DPS422XTSA1";
-	return sensor_name;
+	// Implementation;
+	return "DPS422XTSA1";
 }
 void spartan::DPS422XTSA1::printSensorInfo()
 {
@@ -20,22 +20,22 @@ void spartan::DPS422XTSA1::printSensorInfo()
 int spartan::DPS422XTSA1::powerOn()
 { 
 	// Implementation
-	return 0;
+	return RESULT_SUCCESS;
 }
 int spartan::DPS422XTSA1::powerOff()
 { 
 	// Implementation
-	return 0;
+	return RESULT_SUCCESS;
 }
 
 int spartan::DPS422XTSA1::pollData(spartan::MasterDataPacket &dp)
 {
 	// Implementation
-	return 0;
+	return RESULT_SUCCESS;
 }
 
 int spartan::DPS422XTSA1::printValues() const
 {
 	// Implementation
-	return 0;
+	return RESULT_SUCCESS;
 }
